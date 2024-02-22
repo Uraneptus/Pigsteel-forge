5aff5a08409c7895c489007c1896ef1a8c56de28
Uraneptus <50745606+Uraneptus@users.noreply.github.com>
2024-02-23 00:55:28 +0100

    Axe & Honey

--- src/main/java/com/uraneptus/pigsteel/common/blocks/Zombifiable.java
+++ src/main/java/com/uraneptus/pigsteel/common/blocks/Zombifiable.java
@@ -4,21 +4,36 @@ import com.google.common.base.Suppliers;
 import com.google.common.collect.BiMap;
 import com.uraneptus.pigsteel.core.other.tags.PigsteelBlockTags;
 import com.uraneptus.pigsteel.core.registry.PigsteelBlocks;
+import net.minecraft.advancements.CriteriaTriggers;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.util.RandomSource;
 import net.minecraft.util.StringRepresentable;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.HoneycombItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.ChangeOverTimeBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.MapColor;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraftforge.common.ToolAction;
+import net.minecraftforge.common.ToolActions;
+import net.minecraftforge.common.extensions.IForgeBlock;
 
 import java.util.Optional;
 import java.util.function.Supplier;
 
 public interface Zombifiable extends ChangeOverTimeBlock<Zombifiable.ZombificationLevel> {
     Supplier<BiMap<Block, Block>> PREVIOUS_BY_BLOCK = Suppliers.memoize(() -> PigsteelBlocks.NEXT_BY_BLOCK.get().inverse());
+    Supplier<BiMap<Block, Block>> PREVIOUS_WAXED_BY_BLOCK = Suppliers.memoize(() -> PigsteelBlocks.NEXT_WAXED_BY_BLOCK.get().inverse());
     float blockInfluence = 0.1F;
     float baseChance = 0.9F;
 
@@ -87,8 +102,42 @@ public interface Zombifiable extends ChangeOverTimeBlock<Zombifiable.Zombificati
         return Optional.ofNullable(PREVIOUS_BY_BLOCK.get().get(state.getBlock())).map((block) -> block.withPropertiesOf(state));
     }
 
+    static Optional<BlockState> getPreviousWaxed(BlockState state) {
+        return Optional.ofNullable(PREVIOUS_WAXED_BY_BLOCK.get().get(state.getBlock())).map((block) -> block.withPropertiesOf(state));
+    }
+
     default boolean canRandomTick(BlockState state) {
         return Optional.ofNullable(PigsteelBlocks.NEXT_BY_BLOCK.get().get(state.getBlock())).isPresent() && !PigsteelBlocks.NEXT_WAXED_BY_BLOCK.get().containsValue(state.getBlock());
+    }
+
+    static InteractionResult applyWax(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult result) {
+        ItemStack itemInHand = player.getItemInHand(hand);
+        if (itemInHand.getItem() instanceof HoneycombItem) {
+            if (PigsteelBlocks.NEXT_WAXED_BY_BLOCK.get().containsKey(state.getBlock())) {
+                if (player instanceof ServerPlayer serverPlayer) {
+                    CriteriaTriggers.ITEM_USED_ON_BLOCK.trigger(serverPlayer, pos, itemInHand);
+                }
+                level.setBlock(pos, PigsteelBlocks.NEXT_WAXED_BY_BLOCK.get().get(state.getBlock()).withPropertiesOf(state), 11);
+                level.playSound(player, pos, SoundEvents.HONEYCOMB_WAX_ON, SoundSource.BLOCKS, 1.0F, 1.0F);
+                level.levelEvent(player, 3003, pos, 0);
+
+                if (!player.isCreative() && !player.getAbilities().instabuild) {
+                    itemInHand.shrink(1);
+                }
+                return InteractionResult.sidedSuccess(true);
+            }
+        }
+        return InteractionResult.PASS;
+    }
+
+    default BlockState handleAxe(BlockState returnable, BlockState state, UseOnContext context, ToolAction action, boolean simulate) {
+        if (action == ToolActions.AXE_SCRAPE) {
+            return getPrevious(state).isPresent() ? getPrevious(state).get().getBlock().withPropertiesOf(state) : null;
+        }
+        if (action == ToolActions.AXE_WAX_OFF) {
+            return getPreviousWaxed(state).isPresent() ? getPreviousWaxed(state).get().getBlock().withPropertiesOf(state) : null;
+        }
+        return returnable;
     }
 
     enum ZombificationLevel implements StringRepresentable {
--- src/main/java/com/uraneptus/pigsteel/common/blocks/ZombifiableBlock.java
+++ src/main/java/com/uraneptus/pigsteel/common/blocks/ZombifiableBlock.java
@@ -3,9 +3,18 @@ package com.uraneptus.pigsteel.common.blocks;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.MapColor;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraftforge.common.ToolAction;
+import net.minecraftforge.common.ToolActions;
+import org.jetbrains.annotations.Nullable;
 
 public class ZombifiableBlock extends Block implements Zombifiable {
     private ZombificationLevel zombificationLevel;
@@ -28,6 +37,26 @@ public class ZombifiableBlock extends Block implements Zombifiable {
     @Override
     public boolean isRandomlyTicking(BlockState state) {
         return this.canRandomTick(state);
+    }
+
+    @Override
+    public InteractionResult use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult result) {
+        return Zombifiable.applyWax(state, level, pos, player, hand, result);
+    }
+
+    @Override
+    @Nullable
+    public BlockState getToolModifiedState(BlockState state, UseOnContext context, ToolAction action, boolean simulate) {
+        /*if (action == ToolActions.AXE_SCRAPE) {
+            return Zombifiable.getPrevious(state).isPresent() ? Zombifiable.getPrevious(state).get().getBlock().withPropertiesOf(state) : null;
+        }
+        if (action == ToolActions.AXE_WAX_OFF) {
+            return Zombifiable.getPreviousWaxed(state).isPresent() ? Zombifiable.getPreviousWaxed(state).get().getBlock().withPropertiesOf(state) : null;
+        }
+        return super.getToolModifiedState(state, context, action, simulate);
+
+         */
+        return handleAxe(super.getToolModifiedState(state, context, action, simulate), state, context, action, simulate);
     }
 
     @Override
